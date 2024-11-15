9043fbd5b82933ab29bd2b32aee80e50f0a18829
Andrey Saykin <andrey.saykin1@gmail.com>
2024-11-15 12:11:45 +0100

    only some fixes and adjustments

--- saykin-dev/main.js
+++ saykin-dev/main.js
@@ -3,15 +3,17 @@ const dayArray = ['all', 'monday', 'tuesday', 'wednesday', 'thursday', 'friday',
 $(document).ready(function() {
     const showDaySelect = $('#show-day');
 
+    // Initial setup
     showDay(showDaySelect.val());
     enableDisableNewEntryButton(showDaySelect.val());
 
+    // Event listeners
     showDaySelect.on('change', function() {
         const selectedDay = $(this).val();
         showDay(selectedDay);
         enableDisableNewEntryButton(selectedDay);
     });
-    
+
     $('button.new-entry').on('click', function() {
         if (showDaySelect.val() !== 0) { 
             addRow();
@@ -28,32 +30,47 @@ $(document).ready(function() {
         autoResize({ target: this });
     });
 
-    autoResize({ target: $('#day-activity')})
-
-    function autoResize(event) {
-        const textarea = event.target;
-        textarea.style.height = 'auto';
-        textarea.style.height = textarea.scrollHeight + 'px';
-    }
+    autoResize({ target: $('#day-activity') });
 });
+
+$(document).ready(function() {
+    // Sync desktop view with mobile view
+    $(document).on('input', '.day-row td input, .day-row td textarea', function() {
+        // Get id of the input/textarea element and add 'span' before number
+        const id = $(this).attr('id');
+        const value = $(this).val();
+        const idArray = id.split('-');
+        const day = idArray[0];
+
+        let type, number;
+        if (idArray.length === 4) {
+            type = idArray[1] + '-' + idArray[2];
+            number = idArray[3];
+        } else {
+            type = idArray[1];
+            number = idArray[2];
+        }
+
+        const mobileContent = $('.mobile-element #' + day + '-content-' + number);
+        const mobileElement = mobileContent.find('#' + day + '-' + type + '-span-' + number);
+        mobileElement.text(value);
+
+        console.log('#' + day + '-' + type + '-span-' + number)
+        console.log(mobileElement.attr('id'));
+    });
+});
+
+// Utility functions
+function autoResize(event) {
+    const textarea = event.target;
+    textarea.style.height = 'auto';
+    textarea.style.height = textarea.scrollHeight + 'px';
+}
 
 function getSelectedDay() {
     const showDaySelect = $('#show-day');
     const selectedDay = dayArray[showDaySelect.val()];
-    console.log(selectedDay);
     return selectedDay;
-}
-
-function showDay(day) {
-    const selectedDay = dayArray[day];
-    
-    if (selectedDay == 'all') {
-        $('.day-content').show();
-        return
-    } 
-    
-    $('.day-content').hide();
-    $('.' + selectedDay + '-content').show();
 }
 
 function getNextRowNumber() {
@@ -62,6 +79,29 @@ function getNextRowNumber() {
     return number;
 }
 
+function enableDisableNewEntryButton(day) {
+    const selectedDay = dayArray[day];
+    const newEntryButtons = document.querySelectorAll('button.new-entry');
+
+    newEntryButtons.forEach(button => {
+        button.disabled = (selectedDay === 'all');
+    });
+}
+
+// Show/hide day content
+function showDay(day) {
+    const selectedDay = dayArray[day];
+    
+    if (selectedDay == 'all') {
+        $('.day-content').show();
+        return;
+    } 
+    
+    $('.day-content').hide();
+    $('.' + selectedDay + '-content').show();
+}
+
+// Add new row
 function addRow() {
     const tableRowTemplate = $('#table-row-template').html();
     const selectedDay = getSelectedDay();
@@ -74,26 +114,18 @@ function addRow() {
 
     const table = $('#t-' + selectedDay + ' tbody'); // Select tbody instead of table
 
-    const newRow = tableRowTemplate.replace(/DD/g, selectedDay).replace(/number/g, number);
+    const newRow = tableRowTemplate.replace(/DD/g, selectedDay).replace(/integer/g, number);
 
     const mobileRowTemplate = $('#mobile-row-template').html();
     const container = $('.mobile-element .' + selectedDay + '-content');
 
-    const newMobileRow = mobileRowTemplate.replace(/DD/g, selectedDay).replace(/number/g, number).replace(/Day/g, selectedDay.charAt(0).toUpperCase() + selectedDay.slice(1));
+    const newMobileRow = mobileRowTemplate.replace(/DD/g, selectedDay).replace(/integer/g, number).replace(/Day/g, selectedDay.charAt(0).toUpperCase() + selectedDay.slice(1));
 
     table.append(newRow);
     container.append(newMobileRow);
 }
 
-function enableDisableNewEntryButton(day) {
-    const selectedDay = dayArray[day];
-    const newEntryButtons = document.querySelectorAll('button.new-entry');
-
-    newEntryButtons.forEach(button => {
-        button.disabled = (selectedDay === 'all');
-    });
-}
-
+// Edit popup functions
 var editingDay = null;
 var editingNumber = null;
 
@@ -113,7 +145,7 @@ $('#close-day-edit-popup').on('click', function() {
 });
 
 $('#save-day-edit').on('click', function() {
-    syncDayRow();
+    saveDayRow();
     closeEditPopup();
     resetEditingValues();
 });
@@ -140,16 +172,16 @@ function fetchDayRow() {
     const editPopup = $('#day-edit-popup');
     const dayRow = $('#' + editingDay + '-row-' + editingNumber);
 
-    const activity =  dayRow.find('#' + editingDay +'-activity-' + editingNumber).val();
-    const hours =  dayRow.find('#' + editingDay +'-work-hours-' + editingNumber).val();
-    const department =  dayRow.find('#' + editingDay +'-department-' + editingNumber).val();
+    const activity = dayRow.find('#' + editingDay + '-activity-' + editingNumber).val();
+    const hours = dayRow.find('#' + editingDay + '-work-hours-' + editingNumber).val();
+    const department = dayRow.find('#' + editingDay + '-department-' + editingNumber).val();
 
     editPopup.find('#day-activity').val(activity);
     editPopup.find('#day-work-hours').val(hours);
     editPopup.find('#day-department').val(department);
 }
 
-function syncDayRow() {
+function saveDayRow() {
     const editPopup = $('#day-edit-popup');
     const mobileContent = $('.mobile-element #' + editingDay + '-content-' + editingNumber);
     const dayRow = $('#' + editingDay + '-row-' + editingNumber);
@@ -158,11 +190,11 @@ function syncDayRow() {
     const hours = editPopup.find('#day-work-hours').val();
     const department = editPopup.find('#day-department').val();
 
-    dayRow.find('#' + editingDay +'-activity-' + editingNumber).val(activity);
-    dayRow.find('#' + editingDay +'-work-hours-' + editingNumber).val(hours);
-    dayRow.find('#' + editingDay +'-department-' + editingNumber).val(department);
+    dayRow.find('#' + editingDay + '-activity-' + editingNumber).val(activity);
+    dayRow.find('#' + editingDay + '-work-hours-' + editingNumber).val(hours);
+    dayRow.find('#' + editingDay + '-department-' + editingNumber).val(department);
 
-    mobileContent.find('#' + editingDay +'-activity-span-' + editingNumber).text(activity);
-    mobileContent.find('#' + editingDay +'-work-hours-span-' + editingNumber).text(hours);
-    mobileContent.find('#' + editingDay +'-department-span-' + editingNumber).text(department); 
+    mobileContent.find('#' + editingDay + '-activity-span-' + editingNumber).text(activity);
+    mobileContent.find('#' + editingDay + '-work-hours-span-' + editingNumber).text(hours);
+    mobileContent.find('#' + editingDay + '-department-span-' + editingNumber).text(department);
 }
